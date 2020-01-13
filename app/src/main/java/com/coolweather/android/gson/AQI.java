73e0329812002535d069a82e45f89bdfb1164f36
Qiulp <357867600@qq.com>
2020-01-13 14:18:07 +0800

    加入显示天气信息的功能.

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
